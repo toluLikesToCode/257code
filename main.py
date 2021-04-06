d3b4ed595c20505f6ac38bac19aaee2e71532b58
gaurabacharya <gaurab2008@hotmail.com>
2021-04-06 10:05:54 -0700

    Added imports

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from w1thermsensor import W1ThermSensor
 
 
 def print_hi(name):
