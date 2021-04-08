9d2946ffad46f6ea949e4141e2cddf6169fafdb3
gaurabacharya <gaurab2008@hotmail.com>
2021-04-08 12:10:02 -0700

    new commit

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from datetime import datetime
 import csv
 
 voltage = ["0.3", "0.5", "0.7", "0.9", "2.1", "2.5", "3.0"]
-power = True
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -23,23 +20,23 @@ def print_hi(name):
 
 # function to initialize temp
 def initialize():
-    if read_voltage(voltage[2]) < 0:
+    if read_voltage() < 0:
         temp = 0
     else:
-        temp = read_voltage(voltage[2])
+        temp = read_voltage()
 
     return temp
 
 
 # function to convert voltage to celsius
 def volt_to_celsius(vin):
-    num = 1
+    return float(vin)
 
 
 # function to read voltage uses volt to celsius function and returns temp in celsius
-def read_voltage(voltage):
+def read_voltage():
     # supposed to mimic reading from resistance temp but we included voltage parameter to get a value
-    temp_in_celsius = volt_to_celsius(voltage[4])
+    temp_in_celsius = volt_to_celsius(voltage[1])
     log(temp_in_celsius)
     return temp_in_celsius
 
@@ -47,14 +44,15 @@ def read_voltage(voltage):
 # function to send alert to console probably
 def send_initial_alert(temp):
     log(temp)
-    print("WARNING: Storage unit is 3 degrees away from reaching critical temperature!, dry ice refill is required!")
+    print("WARNING: Storage unit is close to reaching critical temperature!, dry ice refill is required!")
 
 
 # function to read temp for an hour to check near critical temp
-def read_for_hour(temp):
+def read_for_hour():
+    alert_sent = False
     t = time.time()
     while t <= (t + 3600):
-        temp = read_voltage(voltage[1])
+        temp = read_voltage()
         if (-80 > temp > -60) and not alert_sent:
             send_below_critical_alert(temp)
             alert_sent = True
@@ -86,15 +84,16 @@ def log(temp):
 
 
 if __name__ == '__main__':
+    power = True
     critical_temp = (-60, -80)
     initialize()
     while power:
-        temp = read_voltage(voltage[1])
+        temp = read_voltage()
 
-        if temp >= (critical_temp[0] - 3) or temp <= (critical_temp[1] + 3):
+        if (critical_temp[0] >= temp >= critical_temp[0] - 3) or (temp <= critical_temp[1] + 3 and temp <= critical_temp[1]):
             send_initial_alert(temp)
             read_for_hour()
-        elif critical_temp[0] > temp > critical_temp[1]:
+        elif critical_temp[1] > temp > critical_temp[0]:
             send_below_critical_alert(temp)
             read_for_hour()
         else:
