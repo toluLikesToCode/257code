7851ea3ec9e8185680711298dd5f41ab0292022d
gaurabacharya <gaurab2008@hotmail.com>
2021-04-08 10:22:06 -0700

    some functions

--- main.py
+++ main.py
@@ -20,26 +20,30 @@ if __name__ == '__main__':
 #function to initialize temp
 def initialize(self):
 
+    if read_temp() < 0:
+        temp = 0
+    else:
+        temp = read_temp()
 
+    return temp
 #function to convert voltage to celsius
 def volt_to_celsius(self, vin):
 
 
 #function to read voltage uses volt to celsius function and returns temp in celsius
-def read_temp(self):
-
-
+def read_temp(self, voltage):
+    #supposed to mimic reading from resistance temp but we included voltage parameter to get a value
+    return volt_to_celsius(voltage)
 #function to send alert to console probably
-def send_initial_alert(self):
-
-
-#function to check temp
-def check_temp(self):
+def send_initial_alert(self, temp):
+    log(temp)
+    print("WARNING: Storage unit is 3 degrees away from reaching critical temperature!, dry ice refill is required!")
 
 
 #Send warning function
-def send_below_critical_alert(self):
-
+def send_below_critical_alert(self, temp):
+    log(temp)
+    print("STORAGE UNIT IS BELOW CRITICAL TEMPERATURE! REFILL IS REQUIRED!")
 
 #Write down into log (txt) file
 def log(self, temp):
