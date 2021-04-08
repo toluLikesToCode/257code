6f2b8060eeb464fab4c50bd87d427b415a0284c7
gaurabacharya <gaurab2008@hotmail.com>
2021-04-08 11:18:26 -0700

    read for hour added

--- main.py
+++ main.py
@@ -3,54 +3,65 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import time
-from w1thermsensor import W1ThermSensor
+#from w1thermsensor import W1ThermSensor
 from datetime import datetime
 import csv
 
-
+voltage = [0.3, 0.5, 0.7, 0.9, 2.1, 2.5, 3.0]
+power = True
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 #function to initialize temp
-def initialize(self):
+def initialize():
 
-    if read_temp() < 0:
+    if read_voltage(voltage[2]) < 0:
         temp = 0
     else:
-        temp = read_temp()
+        temp = read_voltage(voltage[2])
 
     return temp
 #function to convert voltage to celsius
-def volt_to_celsius(self, vin):
+def volt_to_celsius(vin):
+    num = 1
 
 
 
 #function to read voltage uses volt to celsius function and returns temp in celsius
-def read_voltage(self, voltage):
+def read_voltage(voltage):
     #supposed to mimic reading from resistance temp but we included voltage parameter to get a value
-    temp_in_celsius = volt_to_celsius(voltage)
+    temp_in_celsius = volt_to_celsius(voltage[4])
     log(temp_in_celsius)
     return temp_in_celsius
+
 #function to send alert to console probably
-def send_initial_alert(self, temp):
+def send_initial_alert(temp):
     log(temp)
     print("WARNING: Storage unit is 3 degrees away from reaching critical temperature!, dry ice refill is required!")
 
+#function to read temp for an hour to check near critical temp
+def read_for_hour(temp):
+    t = time.time()
+    while t <= (t+3600):
+        temp = read_voltage(voltage[1])
+        log(temp)
+        time.sleep(5*60)
+
+    
 #Send warning function
-def send_below_critical_alert(self, temp):
+def send_below_critical_alert(temp):
     log(temp)
     print("STORAGE UNIT IS BELOW CRITICAL TEMPERATURE! REFILL IS REQUIRED!")
 
 #Write down into log (txt) file
-def log(self, temp):
+def log(temp):
     f = open("data.csv", "a")
     now = datetime.now()
     is_critical = "N"
@@ -63,6 +74,13 @@ def log(self, temp):
 
     f.write(f'{dt_string},{temp},{is_critical}')
     f.close()
+
+
+if __name__ == '__main__':
+    critical_temp = (-60, -80)
+    initialize()
+    while power:
+        temp = read_voltage(voltage[1])
 
 
 
